c8629f6d71f1c5ef663ce965386d0beda4ccf2d1
demirkilicc <e.berk2004@hotmail.com>
2026-05-20 20:18:19 +0300

    yukleme denemesi

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+#include <optional>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Bomberman");
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+    return 0;
+}
