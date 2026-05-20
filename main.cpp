c90cc2e34e4ec05babd6d38a58449d5275e840c8
demirkilicc <e.berk2004@hotmail.com>
2026-05-20 22:49:49 +0300

    Implemented basic SFML 3 player movement

--- main.cpp
+++ main.cpp
@@ -3,19 +3,47 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Bomberman");
+    sf::RenderWindow window(sf::VideoMode({800,600}),
+"Bomberman");
+window.setFramerateLimit(60);
 
-    while (window.isOpen())
+float playerX = 100.f;
+float playerY = 100.f;
+sf::RectangleShape player({530.f,500.f});
+player.setFillColor(sf::Color::Cyan);
+
+while (window.isOpen())
+{
+    while (const std::optional event =  window.pollEvent())
+ {
+    if (event->is<sf::Event::Closed>())
     {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        window.clear(sf::Color::Black);
-        window.display();
+        window.close();
     }
 
-    return 0;
+    } 
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+    { 
+    playerY -= 3.f;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+    { 
+    playerY += 3.f;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+    { 
+    playerX -= 3.f;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+    { 
+    playerX += 3.f;
+    }
+    player.setPosition({playerX,playerY});
+    window.clear(sf::Color::Black);
+    window.draw(player);
+    window.display();
+}
+
+return 0;
+
 }
